b080b14191fac836936d2348720c0ae94f5b9b3b
Lonax <deniswaren77@gmail.com>
2021-10-13 08:20:15 +0200

    init

---
+++ Summenberechnung/main/Zahlenberechnung.py
@@ -0,0 +1,8 @@
+
+print("Berechnung der Summe von zwei ganzen Zahlen")
+
+zahl1 = int(input("Geben Sie eine Zahl ein:\n"))
+zahl2 = int(input("Geben Sie die zweite Zahl ein:\n"))
+summe = zahl1 + zahl2
+
+print("Die Summe der beiden Zahlen ist", summe)
