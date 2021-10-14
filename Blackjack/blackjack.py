4b692ae5f9d489177fb8f8728ab38f0b38c7cba1
Lonax <deniswaren77@gmail.com>
2021-10-14 12:05:33 +0200

    init

--- Blackjack/blackjack.py
+++ Blackjack/blackjack.py
@@ -3,7 +3,7 @@ import os
 
 name = input("Bitte Geben Sie ihren Namen ein\n")
 string = name
-wuerfel = random.randint(6, 6)
+wuerfel = random.randint(1, 6)
 gesamtsumme_spieler = wuerfel
 gesamtsumme_computer = wuerfel
 
@@ -11,53 +11,40 @@ print("Aktuelle Gesamtaugensumme:", gesamtsumme_spieler)
 
 weiter = input("Möchten Sie weiter Würfeln ? [j/n]\n")
 
-while weiter == "j":
-    wuerfel = random.randint(6, 6)
-    gesamtsumme_spieler = gesamtsumme_spieler + wuerfel
-    print("Aktuelle Gesamtsumme:", gesamtsumme_spieler)
-    weiter = input("Möchten Sie weiter Würfeln ? [j/n]\n")
-    break
-
-if gesamtsumme_spieler < 21:
-    wuerfel = random.randint(1, 6)
-    gesamtsumme_spieler = gesamtsumme_spieler + wuerfel
-    print("Aktuelle Gesamtsumme:", gesamtsumme_spieler)
-    weiter = input("Möchten Sie weiter Würfeln ? [j/n]\n")
-    while weiter == "j":
+if gesamtsumme_spieler < 21 or weiter == "j":
+    while gesamtsumme_spieler < 21:
         wuerfel = random.randint(1, 6)
         gesamtsumme_spieler = gesamtsumme_spieler + wuerfel
         print("Aktuelle Gesamtsumme:", gesamtsumme_spieler)
         weiter = input("Möchten Sie weiter Würfeln ? [j/n]\n")
-        break
-
+        if gesamtsumme_spieler > 21:
+            exit("Du hast Verloren")
 
 
 while gesamtsumme_computer:
-    wuerfel = random.randint(6, 6)
+    wuerfel = random.randint(1, 6)
     gesamtsumme_computer = gesamtsumme_computer + wuerfel
-    if gesamtsumme_computer > 17:
-        print("Es wird verglichen")
-        print("Der Spieler hat", gesamtsumme_spieler, "Augen\n")
-        print("Der Dealer hat", gesamtsumme_computer, "Augen\n")
+    break
 
+    while weiter == "n":
+        if gesamtsumme_computer > 17:
+            print("Es wird verglichen")
+            print("Der Spieler hat", gesamtsumme_spieler, "Augen\n")
+            print("Der Dealer hat", gesamtsumme_computer, "Augen\n")
+            break
         break
 
 if gesamtsumme_spieler == gesamtsumme_computer:
-    print("Die Bank gewinnt")
-    exit(0)
+    exit("Die Bank gewinnt")
+
 if gesamtsumme_spieler > 21:
-    print("Du hast Verloren")
-    exit(0)
+    exit("Du hast Verloren")
+
 if gesamtsumme_computer > 21:
-    print("Du hast Gewonnen")
-    exit(0)
+    exit("Du hast Gewonnen")
+
 if gesamtsumme_computer > gesamtsumme_spieler:
-    print("Du hast Verloren")
-    exit(0)
+    exit("Du hast Verloren")
+
 if gesamtsumme_spieler > gesamtsumme_computer:
-    print("Du hast Gewonnen")
-    exit(0)
-
-
-
-
+    exit("Du hast Gewonnen")
