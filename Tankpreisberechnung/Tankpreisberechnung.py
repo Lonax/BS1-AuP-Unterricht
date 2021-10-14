94846ac2c8539e742a0d49c0a3359adeb5c8ab03
Lonax <deniswaren77@gmail.com>
2021-10-14 09:21:38 +0200

    init

---
+++ Tankpreisberechnung/Tankpreisberechnung.py
@@ -0,0 +1,12 @@
+print("//Berechnung der Tankkosten//")
+print("-----------------------------")
+print("")
+
+K_Liter = float(input("Bitte geben Sie den Kraftstoff in Litern ein:"))
+
+P_Liter = float(input("Bitte geben Sie den Preis pro Liter ein:"))
+
+K_Preis = K_Liter*P_Liter
+
+print("Der Tank hat dich", K_Preis, "Teuronen gekostet")
+
