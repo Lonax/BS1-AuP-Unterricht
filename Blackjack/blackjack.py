c4fd387563c675eede62aeee9dc63dc86dc8a3db
Lonax <deniswaren77@gmail.com>
2021-10-19 11:22:50 +0200

    init

--- Blackjack/blackjack.py
+++ Blackjack/blackjack.py
@@ -3,45 +3,44 @@ import os
 
 name = input("Bitte Geben Sie ihren Namen ein\n")
 string = name
-wuerfel = random.randint(1, 6)
-gesamtsumme_spieler = wuerfel
-gesamtsumme_computer = wuerfel
+wuerfel = int(0)
+gesamtsumme_spieler = int(0)
+gesamtsumme_computer = int(0)
 
-print("Aktuelle Gesamtaugensumme:", gesamtsumme_spieler)
+#print("Aktuelle Gesamtaugensumme:", gesamtsumme_spieler)
 
-weiter = input("Möchten Sie weiter Würfeln ? [j/n]\n")
+#weiter = input("Möchten Sie weiter Würfeln ? [j/n]\n")
 
-if gesamtsumme_spieler < 21 or weiter == "j":
-    while gesamtsumme_spieler < 21:
+while gesamtsumme_spieler < 21:
         wuerfel = random.randint(1, 6)
         gesamtsumme_spieler = gesamtsumme_spieler + wuerfel
         print("Aktuelle Gesamtsumme:", gesamtsumme_spieler)
-        weiter = input("Möchten Sie weiter Würfeln ? [j/n]\n")
+
         if gesamtsumme_spieler > 21:
             exit("Du hast Verloren")
 
+        weiter = input("Möchten Sie weiter Würfeln ? [j/n]\n")
+
+        if weiter == "n":
+            print("Das Würfeln wird beendet")
+            break
+
 
 while gesamtsumme_computer:
-    wuerfel = random.randint(1, 6)
-    gesamtsumme_computer = gesamtsumme_computer + wuerfel
-    break
+        wuerfel = random.randint(1, 6)
+        gesamtsumme_computer = gesamtsumme_computer + wuerfel
 
-    while weiter == "n":
         if gesamtsumme_computer > 17:
+            wuerfel = random.randint(1, 6)
             print("Es wird verglichen")
             print("Der Spieler hat", gesamtsumme_spieler, "Augen\n")
             print("Der Dealer hat", gesamtsumme_computer, "Augen\n")
-            break
-        break
+            if gesamtsumme_spieler > 21:
+                exit('Du hast gewonnen')
+
 
 if gesamtsumme_spieler == gesamtsumme_computer:
     exit("Die Bank gewinnt")
-
-if gesamtsumme_spieler > 21:
-    exit("Du hast Verloren")
-
-if gesamtsumme_computer > 21:
-    exit("Du hast Gewonnen")
 
 if gesamtsumme_computer > gesamtsumme_spieler:
     exit("Du hast Verloren")
