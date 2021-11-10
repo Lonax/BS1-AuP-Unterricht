e6242ad716d499cde546b4d0950ef555c687576f
Lonax <deniswaren77@gmail.com>
2021-11-10 08:15:21 +0100

    init

--- Elektroberechnung/main.py
+++ Elektroberechnung/main.py
@@ -1,6 +1,9 @@
 Volt = float(input("Spannung in Volt"))
 Ampere = float(input("Stromstärke in Ampere"))
-#Ohm = float(input("Widerstand in Ohm"))
-#Watt = float(input("Leistung in Watt"))
+# Ohm = float(input("Widerstand in Ohm"))
+# Watt = float(input("Leistung in Watt"))
 
+Ohm = Volt / Ampere
+Watt = Volt * Ampere
 
+print("Der Strom hat", Ohm, "Widerstand &", Watt, "Watt Stärke")
---
+++ ganzzahl_division_Rest/main.py
@@ -0,0 +1,7 @@
+import numbers
+Zahl = int(input("Zahl"))
+Nenner = int(input("Nenner"))
+
+Ergebniss = Zahl % Nenner
+
+print("Dein Ergebniss ist", Ergebniss)
